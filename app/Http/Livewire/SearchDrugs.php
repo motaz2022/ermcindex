819df5ad09ab82b408d54acd3d602daf92a8a5ff
Motaz Amer <motaz_motaz15@hotmail.com>
2022-06-14 23:53:20 +0200

    bluehist

--- app/Http/Livewire/SearchDrugs.php
+++ app/Http/Livewire/SearchDrugs.php
@@ -22,12 +22,12 @@ class SearchDrugs extends Component
     public function render()
     {
         return view('livewire.search-drugs', [
-            'drugs' => Drug::where('group', 'like', '%'.$this->search.'%')
-            ->orWhere('subgroup', 'like', '%'.$this->search.'%')
-            ->orWhere('scientificname', 'like', '%'.$this->search.'%')
-            ->orWhere('tradename', 'like', '%'.$this->search.'%')
+            'drugs' => Drug::where('group', 'ilike', '%'.$this->search.'%')
+            ->orWhere('subgroup', 'ilike', '%'.$this->search.'%')
+            ->orWhere('scientificname', 'ilike', '%'.$this->search.'%')
+            ->orWhere('tradename', 'ilike', '%'.$this->search.'%')
             ->orderBy('id')
-            ->Simplepaginate(30),
+            ->Simplepaginate(25),
         ]);
     }
 }
